bbe2491120dca93bc4482c865e4a3d784d3ec1b6
nikonov1996 <ibigmeat1996@gmail.com>
2026-07-29 18:13:26 +0700

    at first

---
+++ src/main/java/com/example/autotestsDemoProj/AutotestsDemoProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.autotestsDemoProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutotestsDemoProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutotestsDemoProjApplication.class, args);
+	}
+
+}
